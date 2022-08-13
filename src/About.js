a0f4afdfc9b249ab0faa980f4fa2baf6739d031f
arpanb8907 <58073354+arpanb8907@users.noreply.github.com>
2022-08-13 07:04:21 +0530

    router is added almost done

---
+++ src/About.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function About(props) {
+  return (
+    <div className='container my-3' >
+    <div className="accordion" id="accordionExample" >
+    <div className="accordion-item" >
+      <h2 className="accordion-header" id="headingOne" >
+        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={{backgroundColor:props.mode==='dark'?'#000033' :'white',color:props.mode==='dark'?'white':'black'}}>
+        <strong>what is Text-Buzz?</strong>
+        </button>
+      </h2>
+      <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample" style={{backgroundColor:props.mode==='dark'?'#000033' :'white',color:props.mode==='dark'?'white':'black'}}>
+        <div className="accordion-body" >
+                A single page web application can be used to create text at your own . Application contains various features like
+        uppercase,lowercase converter , Removing extraspace from text , can download as text file in local device and lot more.
+        Live update of number of words and character and preview the written context below makes this one really user friendly
+        </div>
+      </div>
+    </div>
+    <div className="accordion-item">
+      <h2 className="accordion-header" id="headingTwo">
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={{backgroundColor:props.mode==='dark'?'#000033' :'white',color:props.mode==='dark'?'white':'black'}}>
+        <strong>Hope it will be useful</strong>
+        </button>
+      </h2>
+      <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample" style={{backgroundColor:props.mode==='dark'?'#000033' :'white',color:props.mode==='dark'?'white':'black'}}>
+        <div className="accordion-body">
+          <p>made with ❤️</p>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -3,12 +3,36 @@ import './App.css';
 import Navbar from './Navbar';
 import Textbox from './Textbox';
 import Alert from './Alert';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import About from './About';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
 
   const [alert,setalert] = useState(null);
   const [mode,setmode] = useState('light');
+
+//   const [mode, setmode] = useState(()=>{
+
+//     const saved = localStorage.getItem("mode");
+//     const initialValue = JSON.parse(saved);
+//     return initialValue ;
+    
+
+
+// });
+
+
+// useEffect(() => {
+    
+//     localStorage.setItem("mode", JSON.stringify(mode));
+//   }, [mode]);
+
 
   const switchon = ()=>{
 
@@ -42,18 +66,31 @@ function App() {
   return (
     <>
       
-      
+    <Router>
 
       <Navbar title = "Textbuzz" aboutus="About us" mode={mode} switchon={switchon}/>
       
   
       <Alert alert={alert}></Alert>
       
-      <Textbox showalert={showalert} mode={mode}>   </Textbox>
+      <Switch>
+          <Route path="/about">
+            <About mode={mode} switchon={switchon}/>
+          </Route>
+          
+          <Route path="/">
+            <Textbox mode={mode} showalert={showalert}/>
+          
+          </Route>
+      </Switch>
 
+
+
+    
 
       
       
+    </Router>
     </>
     
   );
